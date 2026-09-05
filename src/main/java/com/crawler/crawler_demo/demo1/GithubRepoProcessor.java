daeb2affc8b54df5abf278e1dc907623c19c01dd
何为开心 <13868626+what-is-happiness@user.noreply.gitee.com>
2026-09-05 20:18:42 +0800

    补充注释

--- src/main/java/com/crawler/crawler_demo/demo1/GithubRepoProcessor.java
+++ src/main/java/com/crawler/crawler_demo/demo1/GithubRepoProcessor.java
@@ -7,16 +7,21 @@ import us.codecraft.webmagic.pipeline.ConsolePipeline;
 import us.codecraft.webmagic.processor.PageProcessor;
 
 public class GithubRepoProcessor implements PageProcessor {
+    // 1. 抓取网站的相关配置，包括编码、抓取间隔、重试次数等等
     private final Site site = Site.me()
-            .setRetryTimes(3)
-            .setSleepTime(100);
+            .setRetryTimes(3)   //重试次数
+            .setSleepTime(100); //出去间隔
 
     @Override
+    // 2.爬虫逻辑
     public void process(Page page) {
+        //从页面发现后续的url地址抓取
+
         page.addTargetRequests(page.getHtml().links()
                 .regex("(https://github\\.com/\\w+/\\w+)")
                 .all());
 
+        //定义如何抽取网页信息，保存下来
         page.putField("author", page.getUrl()
                 .regex("https://github\\.com/(\\w+)/.*")
                 .toString());
@@ -33,7 +38,9 @@ public class GithubRepoProcessor implements PageProcessor {
 
         public static void main(String[] args) {
             Spider.create(new GithubRepoProcessor())
+                    //起始url
                     .addUrl("https://github.com/code4craft")
+                    //保存格式
                     .addPipeline(new ConsolePipeline())
                     .thread(5)
                     .run();
