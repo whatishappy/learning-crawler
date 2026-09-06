6023d6567181a16dc2aeb59b7b2bf87a5f1ba2fe
何为开心 <13868626+what-is-happiness@user.noreply.gitee.com>
2026-09-06 13:25:55 +0800

    从元素中获取数据

--- src/test/java/com/crawler/crawler_demo/jsoup/JsoupFirstTest.java
+++ src/test/java/com/crawler/crawler_demo/jsoup/JsoupFirstTest.java
@@ -1,6 +1,7 @@
 package com.crawler.crawler_demo.jsoup;
 
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Attribute;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -42,6 +43,33 @@ public class JsoupFirstTest {
         Elements elementsByClass = document.getElementsByClass("class_a");      //通过class属性获取
 
         Element element = document.getElementsByAttributeValue("href", "http://sh.itcast").first();      //通过属性名和属性值获取
+    }
+
+    @Test
+    public void testData() throws Exception{
+        //获取解析文件，获取Docoumnet
+
+        //根据id获取
+        Document document = Jsoup.parse(new File("F:\\vscode\\js基础.html"));
+
+        //根据id获取元素
+        Element elementById = document.getElementById("test");
+
+        String str = elementById.id();  //从元素中获取id
+
+        String classment = elementById.className();     //从元素中获取class
+
+        String attr = elementById.attr("test");     //获取元素中获取属性值attr
+
+        Attribute aClass = elementById.attribute("class");      //从元素中获取属性值attrbutes
+
+        String text = elementById.text();           //从元素中获取文本内容text
+
+        System.out.println(str);
+        System.out.println(classment);
+        System.out.println(attr);
+        System.out.println(aClass);
+        System.out.println(text);
 
     }
 }
